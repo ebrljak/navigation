80d85285dafd2bd3816eef01b80eb52bcc37db11
Ermina <78378657+ebrljak@users.noreply.github.com>
2023-04-27 18:01:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+(function(){
+    function hideSubmenus() {
+        var submenus = document.querySelectorAll("ul li ul");
+
+        for ( var i = 0; i < submenus; i++) {
+            submenus[i].className = "hide-menu";        }
+    }
+
+    hideSubmenus();
+
+    var menuLinks = document.querySelectorAll(".menulink");
+
+    for(var i = 0; i < menuLinks.length; i++) {
+        menuLinks[i].addEventListener('click', function(event) {
+            event.preventDefault();
+
+            var thisMenu = this.parentNode.querySelector('ul');
+
+            if(thisMenu.classList.contains('hide-menu')) {
+                hideSubmenus();
+                thisMenu.className = "show-menu";
+            } else  {
+                thisMenu.className = "hide-menu";
+            }
+        });
+    }
+})();
